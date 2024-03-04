16183a5a78f2543717d0c98845da9d6e414172fa
Victor Levesque <levesque.vic@gmail.com>
2024-03-04 16:14:52 +1100

    poke routers

---
+++ app/controllers/pokemons_controller.rb
@@ -0,0 +1,11 @@
+class PokemonsController < ApplicationController
+  def index
+
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
---
+++ db/migrate/20240304050052_create_pokemons.rb
@@ -0,0 +1,14 @@
+class CreatePokemons < ActiveRecord::Migration[7.1]
+  def change
+    create_table :pokemons do |t|
+      t.string :nickname
+      t.integer :age
+      t.decimal :rating, precision: 2, scale: 4
+      t.references :user, null: false, foreign_key: true
+      t.string :element
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
