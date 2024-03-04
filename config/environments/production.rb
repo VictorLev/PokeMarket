dd8b1026efc974a21d628ad70ffce62a31f5f6c1
Victor Levesque <levesque.vic@gmail.com>
2024-03-04 15:49:40 +1100

    sign-up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'https://poke-market-9cbc6442a1ce.herokuapp.com/' }
+
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
