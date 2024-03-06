fad6079fb700ebfd66dfcff7a7bcae4d7d16c260
Emma Stone <emma.stone1234@icloud.com>
2024-03-06 11:35:11 +1100

    update pages controller#home with search bar method

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,13 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @pokemons = Pokemon.all
+    # '/search?name="bulbasaur"'
+    # params[:query] = { name: “bulbasaur” }
+    # @pokemons = Pokemon.where(params[:query])
+    if params[:query].present?
+      @pokemons = Pokemon.where("name LIKE ?“, "%{params[:query][:name]}%" )"
+    else
+      @pokemons = Pokemon.all
+    end
   end
 end
