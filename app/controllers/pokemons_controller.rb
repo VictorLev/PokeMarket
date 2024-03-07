841ef4d4d53189d63224ee4a4bd221fdf97ce7f2
Jacopo Riani jacopo.riani@gmail.com <jacopo.riani@gmail.com>
2024-03-07 15:07:05 +1100

    pokemoncreation

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -19,13 +19,16 @@ class PokemonsController < ApplicationController
 
   def create
     @pokemon = Pokemon.new( **pokemon_params, user: current_user)
-    @pokemon.save
-    redirect_to pokemons_path
+    if @pokemon.save
+      redirect_to pokemon_path(@pokemon), notice: "Pokemon successfully created"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
   def pokemon_params
-    params.require(:pokemon).permit(:nickname, :age, :rating, :element, :name)
+    params.require(:pokemon).permit(:nickname, :age, :rating, :element, :name, :photo)
   end
 end
--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,5 +1,7 @@
+require 'open-uri'
+
 class Pokemon < ApplicationRecord
-  has_one_attached :photo
+  before_validation :fetch_img_url
   belongs_to :user
   has_many :rental, dependent: :destroy
 
@@ -14,4 +16,18 @@ class Pokemon < ApplicationRecord
     }
 
   multisearchable against: [:name, :element, :rating]
+
+  private
+
+  def fetch_img_url
+    # Call the api using name of Pokemon
+    url = "https://pokeapi.co/api/v2/pokemon/#{self.name}/"
+    pokemon_serialized = URI.open(url).read
+    api_return = JSON.parse(pokemon_serialized)
+    # Grab the imgUrl and add as attribute to the Pokemon
+    img_url = api_return["sprites"]["front_default"]
+    self.imageUrl = img_url
+  rescue
+    errors.add(:base, "This pokemon does not exist...")
+  end
 end
